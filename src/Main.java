c3ebdd20bca4356b6f0bf64c8e77c8ac625ce9f8
Parv Shibu <parv4344@gmail.com>
2026-01-16 23:28:20 +0530

    LAB 1 ASSIGNMENT (EDITED)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
-  public static void main(String[] args) {
-    int x = 5;
-    int y = 3;
-    System.out.println(x + y);
-  }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a + b);
+        sc.close();
+    }
 }
